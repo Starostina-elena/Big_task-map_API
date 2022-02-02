1e3f685c9f22631171c278906df83f3d0043ac90
Лена <starostina.lena05@gmail.com>
2022-02-02 14:19:49 +0400

    2 и 3 задача домашней работы - поиск адреса, размещение метки на этом адресе, сохранение метки при смене карты. Добавлена папка Samples

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from PIL import Image, ImageQt
 
 from main_ui import Ui_MainWindow
 
+from Samples import geocoder
+
 
 class Map(QMainWindow, Ui_MainWindow):
 
@@ -19,20 +21,21 @@ class Map(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         self.spn_size = 0.065
-
         self.ll_size = [46.034158, 51.533103]
-
         self.ll = f'{self.ll_size[0]},{self.ll_size[1]}'
-
         self.spn = (str(self.spn_size) + ',' + str(self.spn_size))
 
+        self.point_coords = None  # хранит координаты метки
+
         self.show_map(self.ll, self.spn)
+
+        self.btn_search_place.clicked.connect(self.search_place_with_name)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
             self.spn_size /= 2
-            if self.spn_size < 0.0005078125:
-                self.spn_size = 0.0005078125
+            if self.spn_size < 0.00005078125:
+                self.spn_size = 0.00005078125
             spn = (str(self.spn_size) + ',' + str(self.spn_size))
             ll = f'{self.ll_size[0]},{self.ll_size[1]}'
             self.show_map(ll, spn)
@@ -69,11 +72,32 @@ class Map(QMainWindow, Ui_MainWindow):
             spn = (str(self.spn_size) + ',' + str(self.spn_size))
             self.show_map(ll, spn)
 
+    def search_place_with_name(self):
+        """Находит место по названию в поле для ввода"""
+
+        if self.input_place_name.text():
+            self.ll, self.spn = geocoder.get_ll_span(self.input_place_name.text())
+            self.spn_size = min(list(map(float, self.spn.split(','))))
+            self.ll_size = list(map(float, self.ll.split(',')))
+
+            self.point_coords = self.ll_size.copy()
+
+            self.show_map(self.ll, self.spn)
+
+            self.btn_search_place.clearFocus()
+
     def show_map(self, ll, spn, map_type='map', params=None):
         # ll и spn в формате "<число>,<число>" (да-да, для spn тоже два числа),
         # map_type - строка, params - словарь, все ключи и значения - строки
 
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={ll}&spn={spn}&l={map_type}&size=650,450"
+
+        if not params and self.point_coords:
+            params = {
+                'pt': f'{self.point_coords[0]},{self.point_coords[1]},comma'
+            }
+        if params and self.point_coords and 'pt' not in params:
+            params['pt'] = f'{self.point_coords[0]},{self.point_coords[1]},comma'
 
         response = requests.get(map_request, params=params)
 
